e05049fc4efba6624a4f42d8f72a25e6bd58beb6
LEANDRO DOS SANTOS ARAUJO <leandro.araujo@alunos.sc.senac.br>
2025-08-12 21:33:46 -0300

    Adicionando entidades, repositórios, controllers e services

---
+++ src/main/java/com/Entra21/Buzix/repositories/BusStopRepository.java
@@ -0,0 +1,12 @@
+package com.Entra21.Buzix.repositories;
+
+
+import com.Entra21.Buzix.entities.BusStop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusStopRepository extends JpaRepository<BusStop, Long> {
+}
+
+}
