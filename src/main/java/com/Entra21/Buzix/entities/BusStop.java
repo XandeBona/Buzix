8db00624cf2fd3a86057c6ae34f0a55fdb2010d2
ALEXANDRE GABRIEL BONA <alexandre.bona3@alunos.sc.senac.br>
2025-08-13 20:37:03 -0300

    Ajustes na classe BusStop

--- src/main/java/com/Entra21/Buzix/entities/BusStop.java
+++ src/main/java/com/Entra21/Buzix/entities/BusStop.java
@@ -1,48 +1,57 @@
-//package com.Entra21.Buzix.entities;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//
-//@Entity
-//public class BusStop {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//    private Double latitude;
-//    private Double longitude;
-//
-//    public BusStop() {
-//
-//    }
-//
-//    public BusStop(Integer id, Double latitude, Double longitude) {
-//        this.id = id;
-//        this.latitude = latitude;
-//        this.longitude = longitude;
-//    }
-//
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public Double getLatitude() {
-//        return latitude;
-//    }
-//
-//    public void setLatitude(Double latitude) {
-//        this.latitude = latitude;
-//    }
-//
-//    public Double getLongitude() {
-//        return longitude;
-//    }
-//
-//    public void setLongitude(Double longitude) { this.longitude = longitude;
-//    }
-//}
+package com.Entra21.Buzix.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "busstops")
+public class BusStop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String identifier;
+    private Double latitude;
+    private Double longitude;
+
+    public BusStop() {
+
+    }
+
+    public BusStop(Integer id, String identifier, Double latitude, Double longitude) {
+        this.id = id;
+        this.identifier = identifier;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
--- src/main/java/com/Entra21/Buzix/repositories/BusStopRepository.java
+++ src/main/java/com/Entra21/Buzix/repositories/BusStopRepository.java
@@ -1,11 +1,9 @@
-//package com.Entra21.Buzix.repositories;
-//
-//
-//import com.Entra21.Buzix.entities.BusStop;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface BusStopRepository extends JpaRepository<BusStop, Integer> {
-//}
-//
+package com.Entra21.Buzix.repositories;
+
+
+import com.Entra21.Buzix.entities.BusStop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusStopRepository extends JpaRepository<BusStop, Integer> {
+}
+
