175efd241544d41ad2b9021c301b640d4abb6f55
Alexandre <alexandregabrielbona@gmail.com>
2025-08-26 00:15:31 -0300

    Adicionada funcao de cadastro em massa com arquivo KMZ

--- src/main/resources/static/js/ponto.js
+++ src/main/resources/static/js/ponto.js
@@ -1,3 +1,50 @@
+async function cadastrarEmMassa(file) {
+    if (!file) {
+        alert("Selecione um arquivo KMZ!");
+        return;
+    }
+
+    try {
+        const arrayBuffer = await file.arrayBuffer();
+        const zip = await JSZip.loadAsync(arrayBuffer);
+
+        //Busca o KML dentro do KMZ
+        const kmlFile = Object.keys(zip.files).find(name => name.endsWith(".kml"));
+        if (!kmlFile) throw new Error("Nenhum KML encontrado no KMZ!");
+
+        const kmlText = await zip.files[kmlFile].async("text");
+
+        //Converte KML
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(kmlText, "application/xml");
+
+        const placemarks = xmlDoc.getElementsByTagName("Placemark");
+
+        for (let placemark of placemarks) {
+            const pointName  = placemark.getElementsByTagName("name")[0]?.textContent;
+            const coordsText = placemark.getElementsByTagName("coordinates")[0]?.textContent.trim();
+
+            if (coordsText) {
+                //Busca Lon e Lat (se Altitude existir será ignorado)
+                const [longitude, latitude] = coordsText.split(",").map(Number);
+
+                await fetch("/busstops/register", {
+                    method: "POST",
+                    credentials: "include",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ identifier: pointName , latitude, longitude })
+                });
+            }
+        }
+
+        alert("Cadastro em massa concluído com sucesso!");
+
+    } catch (err) {
+        console.error(err);
+        alert("Erro ao processar o arquivo KMZ");
+    }
+}
+
 function cadastrarPonto() {
     const inputIdentifier = document.getElementById("input_identifier");
     const inputLatitude = document.getElementById("input_latitude");
@@ -40,6 +87,20 @@ function setupEvents() {
     document.getElementById("form-busStop").addEventListener("submit", function (event) {
         event.preventDefault();
         cadastrarPonto();
+    });
+
+    //Para envio em massa
+    const fileInput = document.getElementById("file");
+    const fileForm = fileInput.closest("form");
+
+    fileForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        cadastrarEmMassa(fileInput.files[0]);
+    });
+
+    //Mostra o nome do arquivo selecionado
+    fileInput.addEventListener("change", function () {
+        document.getElementById("file-name").textContent = fileInput.files[0]?.name || "Nenhum arquivo escolhido";
     });
 }
 
